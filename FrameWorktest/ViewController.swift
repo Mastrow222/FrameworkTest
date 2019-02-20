98d45c0136ea45b9b230af4efea97436ebf05108
Ventuno Technologies <ventuno@ventunoLunar.local>
2019-02-20 19:26:44 +0530

    ADD

---
+++ FrameWorktest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FrameWorktest
+//
+//  Created by Ventuno Technologies on 20/02/19.
+//  Copyright © 2019 Ventuno Technologies. All rights reserved.
+//
+
+import UIKit
+import TestPod
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        var str : TestPodFile?
+        
+        print(str!.show())
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
